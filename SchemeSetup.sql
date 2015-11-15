264952340c2bd8677bbff53791e27a9c50020af3
Emily <emilyrohrbough@yahoo.com>
2015-11-15 15:17:41 -0700

    Added Comments & Removed Generated Errors

--- SchemeSetup.sql
+++ SchemeSetup.sql
@@ -18,31 +18,34 @@ CREATE SCHEMA IF NOT EXISTS `Recipes` DEFAULT CHARACTER SET utf8 COLLATE utf8_ge
 USE `Recipes` ;
 
 -- -----------------------------------------------------
--- Table `Recipes`.`INSTRUCTION_LIST`
+-- Table `Recipes`.`INSTRUCTION_LIST` 
+-- #INSTRUCTION_LIST
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`INSTRUCTION_LIST` (
 	`Direction_No` INT NOT NULL COMMENT '',
 	`Directions` LONGTEXT NOT NULL COMMENT '',
 	`Prep_Time` VARCHAR(45) NULL COMMENT '',
 	`Difficulty` INT NULL COMMENT '',
-	PRIMARY KEY (`Direction_No`)  COMMENT '')
+	PRIMARY KEY (`Direction_No`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`AMOUNT_REQUIRED`
+-- #AMOUNT_REQUIRED
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`AMOUNT_REQUIRED` (
 	`Recipe_No` INT NOT NULL COMMENT '',
 	`Ingredient_No` INT NOT NULL COMMENT '',
 	`Amount` DOUBLE NOT NULL COMMENT '',
 	`Unit` VARCHAR(10) NOT NULL COMMENT '',
-	PRIMARY KEY (`Recipe_No`, `Ingredient_No`)  COMMENT '')
+	PRIMARY KEY (`Recipe_No`, `Ingredient_No`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`SOURCE`
+-- #SOURCE
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`SOURCE` (
 	`Source_No` INT NOT NULL COMMENT '',
@@ -50,12 +53,13 @@ CREATE TABLE IF NOT EXISTS `Recipes`.`SOURCE` (
 	`Reference` VARCHAR(45) NOT NULL COMMENT 'Type should be like: website, blog, cook book, home recipe, etc',
 	`Type` VARCHAR(45) NOT NULL COMMENT '',
 	`Author-optional` VARCHAR(45) NULL COMMENT '',
-	PRIMARY KEY (`Source_No`)  COMMENT '')
+	PRIMARY KEY (`Source_No`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`RECIPE`
+-- #RECIPE
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`RECIPE` (
 	`Recipe_No` INT NOT NULL COMMENT '',
@@ -66,9 +70,9 @@ CREATE TABLE IF NOT EXISTS `Recipes`.`RECIPE` (
 	`Photo` VARCHAR(45) NULL COMMENT '',
 	`Direction_No` INT NOT NULL COMMENT '',
 	`Source_No` INT NOT NULL COMMENT '',
-	INDEX `Direction_No_idx` (`Direction_No` ASC)  COMMENT '',
-	PRIMARY KEY (`Recipe_No`)  COMMENT '',
-	INDEX `Source_No_idx` (`Source_No` ASC)  COMMENT '',
+	INDEX `Direction_No_idx` (`Direction_No` ASC),
+	PRIMARY KEY (`Recipe_No`),
+	INDEX `Source_No_idx` (`Source_No` ASC),
 	CONSTRAINT `Direction_No`
 		FOREIGN KEY (`Direction_No`)
 		REFERENCES `Recipes`.`INSTRUCTION_LIST` (`Direction_No`)
@@ -89,6 +93,7 @@ ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`NUTRITIONAL_FACTS`
+-- #NURTRUITIONAL_FACTS
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`NUTRITIONAL_FACTS` (
 	`Nutrition_No` INT NOT NULL COMMENT '',
@@ -97,23 +102,25 @@ CREATE TABLE IF NOT EXISTS `Recipes`.`NUTRITIONAL_FACTS` (
 	`Sugar` DOUBLE NULL COMMENT '',
 	`Sodium` DOUBLE NULL COMMENT '',
 	`Fat` DOUBLE NULL COMMENT '',
-	PRIMARY KEY (`Nutrition_No`)  COMMENT '')
+	PRIMARY KEY (`Nutrition_No`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`MEASUREMENT_CONVERSION`
+-- #MEASUREMNT_CONVERSION
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`MEASUREMENT_CONVERSION` (
 	`Unit` VARCHAR(10) NOT NULL COMMENT '',
 	`Standard_Unit` VARCHAR(10) NOT NULL COMMENT '',
 	`Unit_to_Standard_Value` FLOAT NOT NULL COMMENT '',
-	PRIMARY KEY (`Standard_Unit`, `Unit`)  COMMENT '')
+	PRIMARY KEY (`Standard_Unit`, `Unit`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`INGREDIENT`
+-- #INGREDIENT
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`INGREDIENT` (
 	`Ingredient_No` INT NOT NULL COMMENT '',
@@ -123,9 +130,9 @@ CREATE TABLE IF NOT EXISTS `Recipes`.`INGREDIENT` (
 	`Contains_Dairy` BINARY NOT NULL COMMENT '',
 	`Contains_Glutten` BINARY NOT NULL COMMENT '',
 	`Nutritional_No` INT NOT NULL COMMENT '',
-	UNIQUE INDEX `Nutritional_No_UNIQUE` (`Nutritional_No` ASC)  COMMENT '',
-	PRIMARY KEY (`Ingredient_No`)  COMMENT '',
-	INDEX `Amount_Required_idx` (`Ingredient_No` ASC)  COMMENT '',
+	UNIQUE INDEX `Nutritional_No_UNIQUE` (`Nutritional_No` ASC) ,
+	PRIMARY KEY (`Ingredient_No`),
+	INDEX `Amount_Required_idx` (`Ingredient_No` ASC),
 	CONSTRAINT `Amount_Required`
 		FOREIGN KEY (`Ingredient_No`)
 		REFERENCES `Recipes`.`AMOUNT_REQUIRED` (`Ingredient_No`)
@@ -141,11 +148,12 @@ ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`COOKWARE`
+-- #COOKWARE
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`COOKWARE` (
 	`Direction_No` INT NOT NULL COMMENT '',
 	`Cookware` VARCHAR(45) NOT NULL COMMENT '',
-	PRIMARY KEY (`Cookware`, `Direction_No`)  COMMENT '',
+	PRIMARY KEY (`Cookware`, `Direction_No`),
 	CONSTRAINT `Instruction_List`
 		FOREIGN KEY (`Direction_No`)
 		REFERENCES `Recipes`.`INSTRUCTION_LIST` (`Direction_No`)
@@ -156,11 +164,12 @@ ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `Recipes`.`VITAMIN`
+-- #VITAMIN
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Recipes`.`VITAMIN` (
 	`Nutrition_No` INT NOT NULL COMMENT '',
 	`Vitamin` VARCHAR(45) NOT NULL COMMENT '',
-	PRIMARY KEY (`Vitamin`, `Nutrition_No`)  COMMENT '',
+	PRIMARY KEY (`Vitamin`, `Nutrition_No`),
 	CONSTRAINT `Nutritional_Facts`
 		FOREIGN KEY (`Nutrition_No`)
 		REFERENCES `Recipes`.`NUTRITIONAL_FACTS` (`Nutrition_No`)
